a0657bc098accd8e9205f8b92274d04ce2d7f9fb
thakurlal123 <114184268+thakurlal123@users.noreply.github.com>
2022-09-23 00:51:31 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+public class Typecasting {
+    public static  void main(String[] args) {
+        //int to float
+        int n=5;
+        int m =2;
+        float sum=n/m;
+        System.out.println("the value of sum = "+sum);
+        float add =(float)n/m;
+        System.out.println("the value of add = "+add);
+        
+
+
+    }
+    
+}
